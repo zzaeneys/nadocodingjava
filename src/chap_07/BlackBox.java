31a5d8da97fd397a2b7777fb7b1191694f8474f3
zzaeneys <zkk054@naver.com>
2023-01-20 00:37:56 +0900

    commit

---
+++ src/chap_07/BlackBox.java
@@ -0,0 +1,64 @@
+package chap_07;
+
+public class BlackBox {
+    // 필드
+    String modelName; // 모델명
+    String resolution; // 해상도
+    int price; // 가격
+    String color; // 색상
+    static boolean canAutoReport = false; // 자동 신고 기능
+
+    // 생성자
+    BlackBox() {
+        System.out.println("기본 생성자 호출");
+    }
+    // 메소드
+    void autoReport() {
+        if (canAutoReport) {
+            System.out.println("충돌이 감지되어 자동으로 신고합니다.");
+        } else {
+            System.out.println("자동 신고 기능이 지원되지 않습니다.");
+        }
+    }
+
+    void insertMemoryCard(int capacity) {
+        System.out.println("메모리 카드가 삽입되었습니다.");
+        System.out.println("용량은 " + capacity + "GB 입니다.");
+    }
+
+    int getVideoFileCount(int type) {
+        if (type == 1) { // 일반 영상
+            return 9;
+        } else if (type == 2) { // 이벤트 영상
+            return 1;
+        }
+        return 10;
+    }
+
+    // showDataTime : 날짜정보 표시여부
+    // showSpeed : 속도정보 표시여부
+    // min : 영상 기록 단위(분)
+    void record(boolean showDataTime, boolean showSpeed, int min) {
+        System.out.println("녹화를 시작합니다.");
+        if (showDataTime) {
+            System.out.println("영상에 날짜정보가 표시됩니다.");
+        }
+        if (showSpeed) {
+            System.out.println("영상에 속도정보가 표시됩니다.");
+        }
+        System.out.println("영상은 " + min + "분 단위로 기록됩니다.");
+    }
+
+    void record() {
+        record(true, true, 5);
+    }
+
+    static void callServiceCenter() {
+        System.out.println("서비스 센터(1588-0000) 로 연결합니다.");
+        // modelName = "test";
+    }
+
+    void appendModelName(String modelName) {
+        this.modelName = modelName;
+    }
+}
---
+++ src/chap_07/_08_Constructor.java
@@ -0,0 +1,7 @@
+package chap_07;
+
+public class _08_Constructor {
+    public static void main(String[] args) {
+        BlackBox b1 = new BlackBox();
+    }
+}
